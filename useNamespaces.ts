77b15b3ae1c045d081c12a9dfa7907ab1830fb5e
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-11 16:45:06 +0700

    ChadUI

--- useNamespaces.ts
+++ useNamespaces.ts
@@ -1,6 +1,11 @@
+import { i18n } from 'i18next'
 import { useTranslation } from 'next-i18next'
 
 export function useNamespaces(namespaces: string[]) {
   const { i18n } = useTranslation(namespaces)
+  return mapNamespaces(i18n, namespaces)
+}
+
+export function mapNamespaces(i18n: i18n, namespaces: string[]) {
   return namespaces.map(n => i18n.getFixedT(null, n))
 }
